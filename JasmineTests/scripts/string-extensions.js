10a949355cf95376aa9f6105ac70057b91c8aad5
Mike Brown <boneclot@gmail.com>
2016-11-11 12:58:22 +0000

    JS string formating: first attempt. 2 failing whitespace tests outstanding.

--- JasmineTests/scripts/string-extensions.js
+++ JasmineTests/scripts/string-extensions.js
@@ -1,4 +1,25 @@
-﻿String.prototype.capitalise = function () {}
-String.prototype.camelCaseToSpineCase = function () { }
-String.prototype.spineCaseToCamelCase = function () { }
-String.prototype.format = function () { }
+﻿String.prototype.capitalise = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+String.prototype.camelCaseToSpineCase = function () {
+    return this.replace(/([A-Z])/g, function ($1) { return "-" + $1.toLowerCase(); });
+}
+
+String.prototype.spineCaseToCamelCase = function () {
+
+    var temp = this.replace(/(-)(\w)/g, function ($1) { return $1.toUpperCase(); });
+
+    return temp.replace(/-/g, '');
+}
+
+String.prototype.format = function () {
+    // http://stackoverflow.com/questions/18405736/is-there-a-c-sharp-string-format-equivalent-in-javascript
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+        ;
+    });
+}
